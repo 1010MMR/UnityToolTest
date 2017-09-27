b43d324b8be6b47d2c2e01da5ade8f6667821ff5
1010MMR <sdpect@naver.com>
2017-09-27 17:42:09 +0900

    Add

---
+++ Assets/Scripts/Base/Table.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+using UnityEngine;
+
+public class Table {
+    public virtual void Parse(Dictionary<string, string> list) { }
+
+    public void Load(string path)
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>(path);
+        if (textAsset != null)
+        {
+            string[] inputDataArray = textAsset.text.Split('\n');
+            string[] headerDataArray = inputDataArray[0].Split(',');
+
+            for (int i = 1; i < inputDataArray.Length; i++)
+            {
+                string[] lineDataArray = inputDataArray[i].Split(',');
+                if (lineDataArray.Length.Equals(0) == false)
+                {
+                    Dictionary<string, string> dataTable = new Dictionary<string, string>();
+                    for (int j = 0; j < lineDataArray.Length; j++)
+                    {
+                        if (headerDataArray[j].Equals("") == false && lineDataArray[j].Equals("") == false)
+                            dataTable.Add(headerDataArray[j], lineDataArray[j]);
+                    }
+
+                    if (dataTable.Count.Equals(0) == false)
+                        Parse(dataTable);
+                }
+            }
+        }
+    }
+
+    public T InitData<T>(Dictionary<string, string> list, T obj)
+    {
+        List<string> keyList = new List<string>(list.Keys);
+
+        for (int i = 0; i < keyList.Count; i++)
+        {
+            PropertyInfo pInfo = typeof(T).GetProperty(keyList[i]);
+            if (pInfo != null)
+                pInfo.SetValue(obj, Convert(pInfo.PropertyType, list[keyList[i]]), null);
+        }
+
+        return obj;
+    }
+
+    public object Convert(System.Type type, string data)
+    {
+        if (type.Equals(typeof(int))) return System.Convert.ToInt32(data);
+        else if (type.Equals(typeof(uint))) return System.Convert.ToUInt32(data);
+        else if (type.Equals(typeof(float))) return System.Convert.ToSingle(data);
+        else if (type.Equals(typeof(bool))) return System.Convert.ToInt32(data).Equals(1);
+        else return System.Convert.ToString(data);
+    }
+}
---
+++ Assets/Scripts/TestTable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTable : Table 
+{
+    [System.Serializable]
+    public class TableData
+    {
+		public int index { get; set; }
+
+		public int title { get; set; }
+
+		public int iValue { get; set; }
+		public float fValue { get; set; }
+    }
+
+    private Dictionary<int, TableData> m_table = null;
+
+    public TestTable() { }
+    public TestTable(string path)
+    {
+        if (m_table == null) m_table = new Dictionary<int, TableData>();
+        else m_table.Clear();
+
+        Load(path);
+    }
+
+    public override void Parse(Dictionary<string, string> list)
+    {
+        TableData data = InitData<TableData>(list, new TableData());
+        m_table.Add(data.index, data);
+    }
+}
